0502c233f19295b16c84b0794145291acfd5e5b8
Ayon Banerjee <ayon6237@gmail.com>
2026-09-25 17:16:51 +0600

    loading animation is added

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const loading = () => {
+    return (
+        <div>
+            <h1>page data is loading...</h1>
+        </div>
+    );
+};
+
+export default loading;
